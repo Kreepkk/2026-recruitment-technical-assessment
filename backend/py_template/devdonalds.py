57705c59ecadb47cc4e66be2ef27897e9044f09c
Kripa <z5591052@ad.unsw.edu.au>
2026-02-12 21:44:48 +1100

    Remove todo part1

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -43,7 +43,6 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: Return name if satisfies all without needing to modify after .strip()
 	recipeName = recipeName.strip()
 	recipeName = recipeName.replace("-", " ")
 	recipeName = recipeName.replace("_", " ")
